8fd37730ef44f48c1be5c8dcc2135afcb67264e5
user <user@user-PC>
2017-09-12 13:59:20 +0900

    1. db url 변경 2. memoVO, candidateVO 추가

---
+++ jobfairy/src/main/java/com/job/fairy/vo/CandidateVO.java
@@ -0,0 +1,48 @@
+package com.job.fairy.vo;
+
+public class CandidateVO {
+	private String userId;
+	private String empUserID;
+	private String resumeNum;
+	private int projectNum;
+	private String regDate;
+	private int recruit;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getEmpUserID() {
+		return empUserID;
+	}
+	public void setEmpUserID(String empUserID) {
+		this.empUserID = empUserID;
+	}
+	public String getResumeNum() {
+		return resumeNum;
+	}
+	public void setResumeNum(String resumeNum) {
+		this.resumeNum = resumeNum;
+	}
+	public int getProjectNum() {
+		return projectNum;
+	}
+	public void setProjectNum(int projectNum) {
+		this.projectNum = projectNum;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	public int getRecruit() {
+		return recruit;
+	}
+	public void setRecruit(int recruit) {
+		this.recruit = recruit;
+	}
+	
+}
---
+++ jobfairy/src/main/java/com/job/fairy/vo/MemoVO.java
@@ -0,0 +1,48 @@
+package com.job.fairy.vo;
+
+public class MemoVO {
+	private int memoNo;
+	private int projectNum;
+	private String resumeNum;
+	private String memo;
+	private String userId;
+	private String regDate;
+	
+	public int getMemoNo() {
+		return memoNo;
+	}
+	public void setMemoNo(int memoNo) {
+		this.memoNo = memoNo;
+	}
+	public int getProjectNum() {
+		return projectNum;
+	}
+	public void setProjectNum(int projectNum) {
+		this.projectNum = projectNum;
+	}
+	public String getResumeNum() {
+		return resumeNum;
+	}
+	public void setResumeNum(String resumeNum) {
+		this.resumeNum = resumeNum;
+	}
+	public String getMemo() {
+		return memo;
+	}
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	
+}
