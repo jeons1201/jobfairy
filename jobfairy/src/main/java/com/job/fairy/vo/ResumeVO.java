a127a0ec5b68093f85f7d55351c3f6afaf28a302
user <user@user-PC>
2017-09-12 16:34:41 +0900

    resumeVO 된다된다된다 제바를ㄴ이ㅏㄹ

---
+++ jobfairy/src/main/java/com/job/fairy/vo/ResumeVO.java
@@ -0,0 +1,129 @@
+package com.job.fairy.vo;
+
+import java.util.Date;
+
+public class ResumeVO {
+	private String resumeNum;	//이력서고유번호.
+	private String aplUserId;	//(구직자)아이디
+	private Date regDate;	//등록일
+	private String photo;	//사진
+	private String userNm;	//이름
+	private String email;	//이메일
+	private String phoneNum;	//핸드폰번호
+	private String telephone;	//집전화
+	private String resident;	//거주지역
+	private String academic;	//학력사항
+	private String career;	//경력사항
+	private String careerTerm;	//경력기간
+	private String jobClass;	//직종
+	private String jobType;	//업종
+	private String license;	// 언어/자격사항
+	private String introduce;	//자기소개
+	private String file;	//첨부파일
+	
+	public String getResumeNum() {
+		return resumeNum;
+	}
+	public void setResumeNum(String resumeNum) {
+		this.resumeNum = resumeNum;
+	}
+	public String getAplUserId() {
+		return aplUserId;
+	}
+	public void setAplUserId(String aplUserId) {
+		this.aplUserId = aplUserId;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getUserNm() {
+		return userNm;
+	}
+	public void setUserNm(String userNm) {
+		this.userNm = userNm;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getResident() {
+		return resident;
+	}
+	public void setResident(String resident) {
+		this.resident = resident;
+	}
+	public String getAcademic() {
+		return academic;
+	}
+	public void setAcademic(String academic) {
+		this.academic = academic;
+	}
+	public String getCareer() {
+		return career;
+	}
+	public void setCareer(String career) {
+		this.career = career;
+	}
+	public String getCareerTerm() {
+		return careerTerm;
+	}
+	public void setCareerTerm(String careerTerm) {
+		this.careerTerm = careerTerm;
+	}
+	public String getJobClass() {
+		return jobClass;
+	}
+	public void setJobClass(String jobClass) {
+		this.jobClass = jobClass;
+	}
+	public String getJobType() {
+		return jobType;
+	}
+	public void setJobType(String jobType) {
+		this.jobType = jobType;
+	}
+	public String getLicense() {
+		return license;
+	}
+	public void setLicense(String license) {
+		this.license = license;
+	}
+	public String getIntroduce() {
+		return introduce;
+	}
+	public void setIntroduce(String introduce) {
+		this.introduce = introduce;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	
+	
+
+}
